e619b5ee9c790945709085d58b8cf3d2adaca2e9
Maksym Uimanov <uimanovmaks@gmail.com>
2026-02-03 12:08:59 +0100

    Refactor Header component: add sticky behavior, backdrop blur, and replace inner div with `<nav>`.

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -4,14 +4,14 @@ import {LinkedinButton} from "@/components/header/LinkedinButton.tsx";
 
 export const Header = () => {
     return (
-        <header className="flex h-15 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-            <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+        <header className="flex h-15 gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) sticky top-0 z-50 backdrop-blur-sm">
+            <nav className="flex w-full gap-1 px-4 lg:gap-2 lg:px-6">
                 <div className="ml-auto flex items-center gap-4">
                     <LinkedinButton />
                     <GithubButton />
                     <ModeToggleButton />
                 </div>
-            </div>
+            </nav>
         </header>
     );
 };
