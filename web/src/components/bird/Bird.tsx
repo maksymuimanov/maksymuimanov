1d763ca78d14578087790982e788df0ba3b3dee8
Maksym Uimanov <uimanovmaks@gmail.com>
2026-02-04 22:28:52 +0100

    Add nested motion animations for Bird component: apply rotation, scaling, and dragging effects

--- web/src/components/bird/Bird.tsx
+++ web/src/components/bird/Bird.tsx
@@ -2,10 +2,18 @@ import { motion } from "motion/react"
 
 export const Bird = () => {
     return (
-        <motion.div drag
-                    whileDrag={{ scale: 1.25, cursor: "grabbing" }}
-                    className="cursor-grab w-17.5 h-17.5 bg-[url(assets/bird.png)] bg-no-repeat bg-cover pixelated">
-            {/*<img className="cursor-grab w-17.5 pixelated" src={bird} alt="Bird :D"></img>*/}
+        <motion.div
+            animate={{ rotate: 720 }}
+            transition={{ ease: "easeInOut", duration: 1 }}>
+            <motion.div
+                animate={{ scale: 1.25 }}
+                transition={{ ease: "easeOut", duration: 0.5, repeat: 1, repeatType: "reverse" }}>
+                <motion.div
+                    drag
+                    whileDrag={{ scale: 1.25, cursor: "grabbing" }}>
+                    <div className="cursor-grab w-20 h-20 bg-[url(assets/bird.png)] bg-no-repeat bg-cover pixelated"></div>
+                </motion.div>
+            </motion.div>
         </motion.div>
     );
 };
